1bcafe3942f2670c36bda55fd9411c5aec01cb04
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-01-04 19:35:31 +0100

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+//======================================== Class Key ============================================
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+// ==============================================================================================
+// ====================================== Class Person ==========================================
+class Person {
+  constructor(private key: Key) {
+    this.key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+// ===============================================================================================
+// ==================================== abstract class House =====================================
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    key.getSignature() === this.key.getSignature() ? this.door = true : console.log('Door closed');
+  }
+}
+// ===============================================================================================
+// ========================================== class MyHouse ======================================
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    key.getSignature() === this.key.getSignature()
+      ? (this.door = true)
+      : console.log("Door closed");
+  }
+}
+
+// ==============================================================================
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +57,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
